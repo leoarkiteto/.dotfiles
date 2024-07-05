6db3ace4c67fc415d4dcd00cab6e712d5ef4f5d6
leo.franca <leo.franca@ustwo.com>
2024-07-05 10:42:11 +0100

    :arrow_up: chore: update icons in LazyVim

---
+++ nvim/.config/nvim/lua/plugins/mini-icons.lua
@@ -0,0 +1,35 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    file = {
+      ["eslint.config.js"] = {
+        glyph = "󰱺",
+        hl = "MiniIconsBlue",
+      },
+      [".eslintrc"] = {
+        glyph = "󰱺",
+        hl = "MiniIconsBlue",
+      },
+      ["eslintrc.json"] = {
+        glyph = "󰱺",
+        hl = "MiniIconsBlue",
+      },
+      [".eslintrc.json"] = {
+        glyph = "󰱺",
+        hl = "MiniIconsBlue",
+      },
+      [".eslintignore"] = {
+        glyph = "󰱺",
+        hl = "MiniIconsBlue",
+      },
+      ["tailwind.config.ts"] = {
+        glyph = "󱏿",
+        hl = "MiniIconsBlue",
+      },
+      ["package.json"] = {
+        glyph = "󰛷",
+        hl = "MiniIconsRed",
+      },
+    },
+  },
+}
