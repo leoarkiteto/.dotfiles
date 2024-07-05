ea215c30a2d63cfdd80e8a1b44f1c9091573ae85
leo.franca <leo.franca@ustwo.com>
2024-07-05 17:52:09 +0100

    :lipstick: style: add mini.icons

--- nvim/.config/nvim/lua/plugins/mini-icons.lua
+++ nvim/.config/nvim/lua/plugins/mini-icons.lua
@@ -1,7 +1,71 @@
 return {
   "echasnovski/mini.icons",
   opts = {
+    filetype = {
+      Dockerfile = {
+        glyph = "󰡨",
+        hl = "MiniIconsBlue",
+      },
+      gitkeep = {
+        glyph = "󰡨",
+        hl = "MiniIconsBlue",
+      },
+    },
+    extension = {
+      ["sh"] = {
+        glyph = "",
+        hl = "MiniIconsGrey",
+      },
+      ["test.ts"] = {
+        glyph = "󰙨",
+        hl = "MiniIconsBlue",
+      },
+      ["test.tsx"] = {
+        glyph = "󰙨",
+        hl = "MiniIconsBlue",
+      },
+      ["test.js"] = {
+        glyph = "󰙨",
+        hl = "MiniIconsYellow",
+      },
+      ["test.jsx"] = {
+        glyph = "󰙨",
+        hl = "MiniIconsYellow",
+      },
+      ["spec.ts"] = {
+        glyph = "󰙨",
+        hl = "MiniIconsBlue",
+      },
+      ["spec.tsx"] = {
+        glyph = "󰙨",
+        hl = "MiniIconsBlue",
+      },
+      ["spec.js"] = {
+        glyph = "󰙨",
+        hl = "MiniIconsYellow",
+      },
+      ["spec.jsx"] = {
+        glyph = "󰙨",
+        hl = "MiniIconsYellow",
+      },
+    },
     file = {
+      [".yarnrc.yml"] = {
+        glyph = "",
+        hl = "MiniIconsBlue",
+      },
+      ["yarn.lock"] = {
+        glyph = "",
+        hl = "MiniIconsBlue",
+      },
+      ["docker-compose.yml"] = {
+        glyph = "󰡨",
+        hl = "MiniIconsBlue",
+      },
+      ["docker-compose.yaml"] = {
+        glyph = "󰡨",
+        hl = "MiniIconsBlue",
+      },
       ["eslint.config.js"] = {
         glyph = "󰱺",
         hl = "MiniIconsBlue",
