92b6e1f00d5e8a6dd458e6e95069c5023aaa07e7
leo.franca <leo.franca@ustwo.com>
2024-09-05 10:26:28 +0100

    :sparkles: feat(LazyVim): add spider-nvim

---
+++ nvim/.config/nvim/lua/plugins/spider.lua
@@ -0,0 +1,23 @@
+return {
+  "chrisgrieser/nvim-spider",
+  keys = {
+    {
+      "w",
+      "<cmd>lua require('spider').motion('w')<CR>",
+      mode = { "n", "o", "x" },
+      desc = "Move to start of next word",
+    },
+    {
+      "e",
+      "<cmd>lua require('spider').motion('e')<CR>",
+      mode = { "n", "o", "x" },
+      desc = "Move to end to next word",
+    },
+    {
+      "b",
+      "<cmd>lua require('spider').motion('b')<CR>",
+      mode = { "n", "o", "x" },
+      desc = "Move to begin of previous word",
+    },
+  },
+}
