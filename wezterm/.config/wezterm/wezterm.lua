7d66c1b6cffbdfcdd0a36a6a9c99e1ca01d5db09
Leo Franca <leoarkiteto@gmail.com>
2024-09-09 21:31:38 +0100

    :sparkles: feat(wezterm): add wezterm config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE", -- disaable the title bar but enalble resizable border
+	default_cursor_style = "BlinkingBar",
+	color_scheme = "One Dark (Gogh)",
+	font = wezterm.font("VictorMono Nerd Font", { weight = "Bold" }),
+	font_size = 15,
+}
+
+return config
