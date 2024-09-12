13006be242d48c47195c054e448b6d1ecbeb8ce7
leo.franca <leo.franca@ustwo.com>
2024-09-12 08:50:21 +0100

    :lipstick: style(wezterm): add custom appearance

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,16 +4,21 @@ local config = wezterm.config_builder()
 
 config = {
 	automatically_reload_config = true,
-	enable_tab_bar = false,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE", -- disaable the title bar but enalble resizable border
+	window_frame = {
+		font = wezterm.font("VictorMono Nerd Font", { weight = "Bold" }),
+		font_size = 14,
+	},
 	default_cursor_style = "BlinkingBar",
+	hide_tab_bar_if_only_one_tab = true,
 	color_scheme = "One Dark (Gogh)",
 	font = wezterm.font("VictorMono Nerd Font", { weight = "Bold" }),
-	font_size = 15,
+	font_size = 17,
+	bold_brightens_ansi_colors = true,
 	window_padding = {
 		left = 8,
-		right = 0,
+		right = 8,
 		top = 8,
 		bottom = 8,
 	},
