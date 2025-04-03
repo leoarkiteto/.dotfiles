77b0a0dc15b750542748929503f25e62ed1e6d91
Leo Franca <leoarkiteto@gmail.com>
2025-04-03 21:38:13 +0100

    ➕chore(LazyVim): Add `avante.nvim` plugin

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,58 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false, -- Never set this value to "*"! Never!
+  opts = {
+    -- add any opts here
+    -- for example
+    provider = "ollama",
+    ollama = {
+      endpoint = "http://127.0.0.1:11434",
+      model = "codellama", -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+      temperature = 0,
+      max_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+      --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+    },
+    cursor_applying_provider = "ollama",
+    behavior = {
+      enable_cursor_planning_mode = true,
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "saghen/blink.cmp", -- autocompletion for avante commands and mentions
+    "echasnovski/mini.icons",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+    "Kaiser-Yang/blink-cmp-avante",
+  },
+}
