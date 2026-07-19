8f1d09399fe2a5641c5db1c705d690d155e79486
Leo Franca <leoarkiteto@gmail.com>
2026-07-19 22:15:18 +0100

    🔥 refactor: Remove dead plugins

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -1,9 +1,3 @@
 return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "olimorris/codecompanion.nvim", enabled = false },
-  { "yetone/avante.nvim", enabled = false },
-  { "ravitemer/mcphub.nvim", enabled = false },
-  { "GustavEikaas/easy-dotnet.nvim", enabled = false },
-  { "akinsho/flutter-tools.nvim", enabled = false
- },
 }
